5e57f43f8f7522af9d481601114ca8ad76282a02
Miguel Angel Gonzales <miguelangel.gonzales@assuresoft.tech>
2025-04-30 11:58:35 -0400

    feature: add .env configs and typing for dotenv.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,7 @@
 import express, {Application, Request, Response} from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
@@ -6,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello!, Typescript + Express!");
+    res.send("Hello!, Typescript + Express! 💯🚀🎯");
 });
 
 app.listen(PORT, () => {
